61cad82e02da92114107c2155e9477b5f59c6b29
chobojajg <60211687+chobojajg@users.noreply.github.com>
2024-03-18 10:39:08 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,5 +1,13 @@
 def solution(num_list):
+    cnt = 0
     for i in num_list:
         if i < 0:
-            return num_list.index(i)
+            return cnt
+        cnt += 1
     return -1
+
+# def solution(num_list):
+#     for i in num_list:
+#         if i < 0:
+#             return num_list.index(i)
+#     return -1
