613da45014480da3768a55e770ec137094bb6c62
chobojajg <60211687+chobojajg@users.noreply.github.com>
2024-03-20 09:09:43 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,2 +1,11 @@
 def solution(array):
     return [max(array), array.index(max(array))]
+
+
+    # answer = [0, 0]
+    # num = array[0]
+    # for i in range(len(array)):
+    #     if num < array[i]:
+    #         num = array[i]
+    #         answer[0] = num
+    #         answer[1] = i
