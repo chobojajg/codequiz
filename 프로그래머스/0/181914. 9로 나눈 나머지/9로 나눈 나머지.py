67bb8ffdccdf81475ea14ebced28ae235cfddede
chobojajg <60211687+chobojajg@users.noreply.github.com>
2024-03-25 08:59:57 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.py
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.py
@@ -1,8 +1,11 @@
-def solution(number):
-    return int(number) % 9
 # def solution(number):
-#     answer = list(map(int, list(number)))
-#     sum_answer = sum(answer)
-#     if sum_answer >= 9:
-#         sum_answer = solution(str(sum_answer))
-#     return sum_answer
+#     return int(number) % 9
+
+def solution(number):
+    answer = list(map(int, list(number)))
+    sum_answer = sum(answer)
+    if sum_answer > 9:
+        sum_answer = solution(str(sum_answer))
+    elif sum_answer == 9:
+        sum_answer = 0
+    return sum_answer
