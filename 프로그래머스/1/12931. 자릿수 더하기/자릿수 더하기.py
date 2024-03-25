f8c3be8fce0d5a568b72a682ae430a25a669c429
chobojajg <60211687+chobojajg@users.noreply.github.com>
2024-03-25 09:51:08 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,6 +1,10 @@
 def solution(n):
-    strarr = list(str(n))
-    numarr = []
-    for i in range(len(strarr)):
-        numarr.append(int(strarr[i]))
-    return sum(numarr)
+    arr = list(map(int, str(n)))
+    return sum(arr)
+    
+# def solution(n):
+#     strarr = list(str(n))
+#     numarr = []
+#     for i in range(len(strarr)):
+#         numarr.append(int(strarr[i]))
+#     return sum(numarr)
