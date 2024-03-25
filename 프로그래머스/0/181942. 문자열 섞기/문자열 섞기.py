0d0705546c6a54d35f3df157d5c0748db8d2c676
chobojajg <60211687+chobojajg@users.noreply.github.com>
2024-03-25 10:16:42 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,11 +1,18 @@
 def solution(str1, str2):
-    num = len(str1)+len(str2)
-    lis = []
-    answer = ''
-    for i in range(num):
-        if i%2 == 0:
-            lis.append(str1[i//2])
-        else:
-            lis.append(str2[i//2])
-    answer = ''.join(lis)
-    return answer
+    answer = []
+    for i in range(len(str1)):
+        answer.append(str1[i])
+        answer.append(str2[i])
+    return ''.join(answer)
+
+# def solution(str1, str2):
+#     num = len(str1)+len(str2)
+#     lis = []
+#     answer = ''
+#     for i in range(num):
+#         if i%2 == 0:
+#             lis.append(str1[i//2])
+#         else:
+#             lis.append(str2[i//2])
+#     answer = ''.join(lis)
+#     return answer
