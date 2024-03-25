ae903d556a150b5cbb3e751c1a1d5d7eee199e22
chobojajg <60211687+chobojajg@users.noreply.github.com>
2024-03-25 09:11:09 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
@@ -1,42 +1,46 @@
 def solution(s):
-    answer = 0
+    return int(s)
+        
+
+# def solution(s):
+#     answer = 0
     
-    sarr = list(s)
-    lista = []
-    signlist = []
-    numlist = []
-    resultlist = []
+#     sarr = list(s)
+#     lista = []
+#     signlist = []
+#     numlist = []
+#     resultlist = []
     
-    if sarr[0] == "-":
-        signlist.append(-1)
-    else:
-        signlist.append(1)
+#     if sarr[0] == "-":
+#         signlist.append(-1)
+#     else:
+#         signlist.append(1)
     
-    for i in range(len(sarr)):
-        print(sarr[i])
-        if sarr[i] == "+":
-            if i == 0:
-                continue
-            numlist.append(int("".join(lista)))
-            signlist.append(1)
-            lista = []
+#     for i in range(len(sarr)):
+#         print(sarr[i])
+#         if sarr[i] == "+":
+#             if i == 0:
+#                 continue
+#             numlist.append(int("".join(lista)))
+#             signlist.append(1)
+#             lista = []
             
-        elif sarr[i] == "-":
-            if i == 0:
-                continue
-            numlist.append(int("".join(lista)))
-            signlist.append(-1)
-            lista = []
+#         elif sarr[i] == "-":
+#             if i == 0:
+#                 continue
+#             numlist.append(int("".join(lista)))
+#             signlist.append(-1)
+#             lista = []
             
-        else:
-            lista.append(sarr[i])
+#         else:
+#             lista.append(sarr[i])
             
-        if i + 1 == len(sarr):
-            numlist.append(int("".join(lista)))
+#         if i + 1 == len(sarr):
+#             numlist.append(int("".join(lista)))
     
-    for i in range(len(numlist)):
-        resultlist.append(numlist[i] * signlist[i])
+#     for i in range(len(numlist)):
+#         resultlist.append(numlist[i] * signlist[i])
     
-    answer = sum(resultlist)
+#     answer = sum(resultlist)
     
-    return answer
+#     return answer
