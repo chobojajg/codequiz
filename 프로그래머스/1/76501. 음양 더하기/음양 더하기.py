cb2b35f115665c09cb9907fd3fa990a45d9af76e
chobojajg <60211687+chobojajg@users.noreply.github.com>
2024-03-25 10:20:19 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,11 +1,8 @@
 def solution(absolutes, signs):
     answer = 0
-    
     for i in range(len(signs)):
         if signs[i]:
             answer = answer + absolutes[i]
         else:
             answer = answer - absolutes[i]
-            
-    
     return answer
